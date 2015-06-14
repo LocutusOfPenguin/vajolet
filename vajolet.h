41cf5590b32ec988779cdd36b366e6a1fc92dc7d
Marco Belli <elcabesa@inwind.it>
2015-06-14 13:55:47 +0200

    2.1.03

--- vajolet.h
+++ vajolet.h
@@ -33,7 +33,7 @@
 //---------------------------------------------
 
 #define PROGRAM_NAME	"Vajolet2"
-#define VERSION			"2.1.02"
+#define VERSION			"2.1.03"
 #define PRECALCULATED_BITSET
 #define HW_BITCOUNT
 
